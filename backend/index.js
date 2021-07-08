1491936df2d033e41af8c4624fea834d6a4673a3
bassamhamdar <hamdar.m.bassam@gmail.com>
2021-07-08 14:36:22 +0300

    fix faq

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const port = 5000;
 const app = express();
+app.use(express.json());
 
 const BandModel = require("./models/Bands");
 const FaqModel = require("./models/Faq");
-app.use(express.json());
-
-
-app.listen(port, () => {
-  console.log(`listening at http://localhost:${port}`);
-});
 
 mongoose.connect(
   "mongodb+srv://lrb:uniquedot@cluster0.nei6t.mongodb.net/BlinkUniters?retryWrites=true&w=majority",
@@ -24,6 +20,12 @@ conn.on("connected", function () {
   console.log("database is connected successfully");
 });
 conn.on("disconnected", function () {
-  console.log("database is disconnected successfully");
+  console.log("database is disconnected ");
 });
 conn.on("error", console.error.bind(console, "connection error:"));
+
+app.use("/", require("./routes/faqRoute"));
+app.use(cors);
+app.listen(port, () => {
+  console.log(`listening at http://localhost:${port}`);
+});
